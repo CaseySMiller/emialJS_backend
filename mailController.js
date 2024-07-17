72cf0fec36c21a5b2cf593b9fb39e8b28097e2ed
CaseySMiller <CaseySMiller@yahoo.com>
2024-07-17 17:21:27 -0600

    added some comments

--- mailController.js
+++ mailController.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 const { Category } = require("./models");
 const axios = require('axios');
 
+// **
+// This requires that api calls from non browser clients be enabled 
+// on the emailjs account in the security section
+// **
+
 const mailController = async (email) => {
   //retrieve and parse the category names
   const categories = await Category.findAll();
@@ -23,8 +28,8 @@ const mailController = async (email) => {
   }
 };
 
+// function to send mail via emailJS API
 const sendMail = async (mailParams) => {
-  // console.log(mailParams);
   const result = await axios.post('https://api.emailjs.com/api/v1.0/email/send', {
     service_id: process.env.EJS_SID,
     template_id: process.env.EJS_TID,
